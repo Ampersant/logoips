6b23f56993f9f0fa63bec23150551ea2e267fac4
Ampersant <pika4yxa1337@gmail.com>
2025-04-30 22:26:54 +0200

    feat(header-menu): add nav to header.blade, add custom Walker class, add scripts

--- web/app/themes/logoips-sage-theme/resources/views/layouts/app.blade.php
+++ web/app/themes/logoips-sage-theme/resources/views/layouts/app.blade.php
@@ -8,6 +8,8 @@
   @php(wp_head())
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">  
+  @stack('styles') 
   @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
@@ -36,5 +38,7 @@
 
   @php(do_action('get_footer'))
   @php(wp_footer())
+  @stack('styles') 
+  @stack('scripts')
 </body>
 </html>
